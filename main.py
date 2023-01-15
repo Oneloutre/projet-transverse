41dc3bb6d185ec93137578617ee8e00bfb1cc750
FuFuLeNarco <valentin.legall55@gmail.com>
2023-01-15 16:25:21 +0100

    Debut_POO

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello World")
-print("coucou")
+class Voiture:
+    nb_voiture = 0
+    def __init__(self, couleur):
+        Voiture.nb_voiture += 1
+        self.couleur = couleur
+    def avancer(self):
+        print("La voiture", self.couleur, "avance vite")
+
+voiture1 = Voiture('Rouge')
+voiture1.avancer()
+voiture2 = Voiture('Jaune')
+voiture2.avancer()
