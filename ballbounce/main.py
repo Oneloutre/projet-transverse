d891bd50dd5f916b3ff4fd9c2d0f8efb885b91a1
FuFuLeNarco <valentin.legall55@gmail.com>
2023-04-17 15:33:57 +0200

    add gnioezuhfz

--- ballbounce/main.py
+++
@@ -1,9 +0,0 @@
-from CreateCircleGrid import returnRayon, createCricleGridMatrix
-from CreateDiamondGrid import createDiamondGridMatrix
-from CreateTriangleGrid import createTriangleGridMatrix
-from displayMatrix import returnDisplayGridMatrixLine
-
-long = int(input('Long'))
-gridMatrix = createDiamondGridMatrix(long)
-for i in range(long):
-    print(returnDisplayGridMatrixLine(gridMatrix, i))
