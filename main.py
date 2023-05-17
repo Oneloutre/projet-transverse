f57d258b1f0c3be2cb4068e29e575047845a261d
Le Gall <valentin.le-gall@efrei.net>
2023-05-17 15:48:47 +0200

    RealFinal

--- main.py
+++ main.py
@@ -14,27 +14,25 @@ background_image = pygame.image.load("assets/FOND_VOLLEYPONG.png")
 background_image = background_image.convert()
 
 while not Models.QUIT:
-    if Models.MENU:
-        Models.MENU = False
-    else:
-        ManageScreen.leave()
-        window.blit(background_image, (0, 0))
-        if Models.player1["points"] < Models.SCORE_MAX and Models.player2["points"] < Models.SCORE_MAX:
-            pygame.draw.rect(window, (255, 0, 0), pygame.Rect(Models.player1["x"], Models.player1["y"], Models.player1["width"],
-                                                              Models.player1["height"]))
-            pygame.draw.rect(window, (255, 0, 0), pygame.Rect(Models.player2["x"], Models.player2["y"], Models.player2["width"],
-                                                              Models.player2["height"]))
-            if Models.ball['timer'] == 0:
-                ManageBall.moveBall()
-            else:
-                ManageScreen.timer3s(window)
-            pygame.draw.circle(window, 255, (Models.ball["x"], Models.ball["y"]),
-                               Models.INITIAL_RADIUS)
-            ManagePlayers.move(Models.player1)
-            ManagePlayers.move(Models.player2)
+    ManageScreen.leave()
+    window.blit(background_image, (0, 0))
+    if Models.player1["points"] < Models.SCORE_MAX and Models.player2["points"] < Models.SCORE_MAX:
+        pygame.draw.rect(window, (255, 0, 0),
+                         pygame.Rect(Models.player1["x"], Models.player1["y"], Models.player1["width"],
+                                     Models.player1["height"]))
+        pygame.draw.rect(window, (255, 0, 0),
+                         pygame.Rect(Models.player2["x"], Models.player2["y"], Models.player2["width"],
+                                     Models.player2["height"]))
+        if Models.ball['timer'] == 0:
+            ManageBall.moveBall()
         else:
-            ManageScreen.displayFinalScore(window)
-            Models.MENU = True
-        ManageScreen.displayOnScreen(window, str(Models.player1["points"]) + " - " + str(Models.player2["points"]), 500, 0,
-                                     (255, 255, 255), 100)
+            ManageScreen.timer3s(window)
+        pygame.draw.circle(window, 255, (Models.ball["x"], Models.ball["y"]),
+                           Models.INITIAL_RADIUS)
+        ManagePlayers.move(Models.player1)
+        ManagePlayers.move(Models.player2)
+    else:
+        ManageScreen.displayFinalScore(window)
+    ManageScreen.displayOnScreen(window, str(Models.player1["points"]) + " - " + str(Models.player2["points"]), 500, 0,
+                                 (255, 255, 255), 100)
     pygame.display.update()
